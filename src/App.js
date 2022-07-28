c955ee7f5ae0af9d7ebd4f0d1664ff5ff71e388b
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-07-28 16:05:28 +0200

    switched to a new branch to separate this app from the main branch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -23,6 +24,7 @@ class App extends Component {
       </div>
     );
   }
+  
 }
 
 export default App;
