18823fbe5ed4b2141c8d1c0f45909e6b316934c8
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-01 11:36:00 +0200

    Section3-33 Mapping Arrays to Elements

--- src/App.js
+++ src/App.js
@@ -7,46 +7,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: 'Stranger', lastName: 'Joe'},
-      company: 'BiffCo'
-    }
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jacky'
+        },
+        {
+          name: 'Billy'
+        }
+      ]
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
-          <button onClick={() => {
-            // We shouldn't set state directly because react will not render it if the obj is the same
-            // this.state.name = 'Gabee';
-            // console.log(this.state);
-
-            // Instead, we should use setState()
-            this.setState(
-              () => { // The first anonymous function is an updater function, the object in this will be used in shallow merge with the state
-                return {
-                  name: {firstName: 'Gabor', lastName: 'Koncz'}
-                } 
-              },
-              () => { // This is a callback that only runs after the update method finished (it is optional)
-                console.log(this.state);
-              }
-            ); // Shallow merging
-          }}
-          >
-            Change name
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map((monster) => { // The first argument of the callback function is the element in the collection
+            return <h1>{monster.name}</h1>;
+          })
+        }
       </div>
     );
   }
