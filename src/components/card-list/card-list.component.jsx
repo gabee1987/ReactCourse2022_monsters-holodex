8f4a3701276f17d3dc9cf990af624bacbc5382a8
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-03 14:51:52 +0200

    Section3-50 CardList Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import './card-list.styles.css';
+import './card.styles.css';
 
 class CardList extends Component {
   render() {
@@ -6,10 +8,24 @@ class CardList extends Component {
 
     return (
       // In a component only one parent element can be present at a time, in this case one div
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className="card-list">
+        {monsters.map((monster) => {
+          // By outsourcing the monster properties to a variable, if any of them changes, we dont need to rename them in the code where we use them
+          // So instead of "monster.name" we can use simply "name"
+          const { name, email, id } = monster;
+
+          return (
+            <div className="card-container" key={id}>
+              <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2`}
+              />
+              {/* With the help of robohash we can get generated pictures by modifying the number in the url */}
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -5,8 +5,9 @@ class SearchBox extends Component {
   render() {
     return (
       // This didn't work: className={'search-box ${this.props.className}'}
+      // Managed to solve the string interpolation problem, it's not a single quatation mark, but a single tick character ...
       <input
-        className={'search-box' + ' ' + this.props.className}
+        className={`search-box ${this.props.className}`}
         type="search"
         placeholder={this.props.placeholder}
         onChange={this.props.onChangeHandler}
