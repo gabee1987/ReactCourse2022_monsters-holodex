f8b1703e2eded9e295f14bd1d758f3a660e21fe2
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-03 15:24:50 +0200

    Section3-50 CardList Component - my solution - Card related code has been organized into a separate Component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './card-list.styles.css';
-import './card.styles.css';
+
+import Card from '../../components/card/card.component.jsx';
 
 class CardList extends Component {
   render() {
@@ -10,21 +11,7 @@ class CardList extends Component {
       // In a component only one parent element can be present at a time, in this case one div
       <div className="card-list">
         {monsters.map((monster) => {
-          // By outsourcing the monster properties to a variable, if any of them changes, we dont need to rename them in the code where we use them
-          // So instead of "monster.name" we can use simply "name"
-          const { name, email, id } = monster;
-
-          return (
-            <div className="card-container" key={id}>
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2`}
-              />
-              {/* With the help of robohash we can get generated pictures by modifying the number in the url */}
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import './card.styles.css';
+
+class Card extends Component {
+  render() {
+    const { monster } = this.props;
+    const { name, email, id } = monster;
+
+    return (
+      // By outsourcing the monster properties to a variable, if any of them changes, we dont need to rename them in the code where we use them
+      // So instead of "monster.name" we can use simply "name"
+      <div className="card-container" key={id}>
+        <img
+          alt={`monster ${name}`}
+          src={`https://robohash.org/${id}?set=set2`}
+        />
+        {/* With the help of robohash we can get generated pictures by modifying the number in the url */}
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
