d0d408a303ff91303001458342097046a7e801e6
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-03 12:16:34 +0200

    Section3-49 CSS in React

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,10 +1,12 @@
 import { Component } from 'react';
+import './search-box.styles.css';
 
 class SearchBox extends Component {
   render() {
     return (
+      // This didn't work: className={'search-box ${this.props.className}'}
       <input
-        className={this.props.className}
+        className={'search-box' + ' ' + this.props.className}
         type="search"
         placeholder={this.props.placeholder}
         onChange={this.props.onChangeHandler}
