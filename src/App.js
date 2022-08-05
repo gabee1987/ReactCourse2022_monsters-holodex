5f90d60c036b60dc4e7a698bf3fce4a7aded865a
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-05 08:22:50 +0200

    Section3-57 Hooks: useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import { useState } from 'react'; // This is a Hook, with this we can encapsulate local state in a functional component
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,67 +7,81 @@ import './App.css';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
-// As a functional component it would look like this ->
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>This is a functional component</h1>
-//     </div>
-//   );
-// };
+// As a functional component it looks like this ->
+const App = () => {
+  const [searchField, setSearchField] = useState(''); // Array destructuring, it gives us back 2 value: [value, setValue]
+  console.log(searchField);
 
-// This is a class component
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      monsters: [],
-      searchfield: '',
-    };
-  }
-
-  // Lifecycle methods
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users') // This will be a promise, thats asynchronous in JS (will going to happen)
-      .then((response) => response.json()) // This is a callback
-      .then((users) =>
-        this.setState(() => {
-          // Function method
-          return { monsters: users };
-        })
-      );
-  }
-
-  // By extracting this function to a named function, instead of using an anonymous one, we got better code readability and performance as well
-  onSearchChange = (event) => {
-    const searchfield = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchfield };
-    });
+  const onSearchChange = (event) => {
+    const searchfieldString = event.target.value.toLowerCase();
+    setSearchField(searchfieldString);
   };
 
-  render() {
-    // We save out these objects to variables, so wont need to use 'this.' all the time
-    const { monsters, searchfield } = this.state;
-    const { onSearchChange } = this;
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder={'search monsters'}
+        className={'monsters-search-box'}
+      />
+      {/* <CardList monsters={filteredMonsters} /> */}
+    </div>
+  );
+};
 
-    const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchfield)
-    );
+// As a class component it looks like this ->
+// class App extends Component {
+//   constructor() {
+//     super();
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder={'search monsters'}
-          className={'monsters-search-box'}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+//     this.state = {
+//       monsters: [],
+//       searchfield: '',
+//     };
+//   }
+
+// Lifecycle methods
+// componentDidMount() {
+//   fetch('https://jsonplaceholder.typicode.com/users') // This will be a promise, thats asynchronous in JS (will going to happen)
+//     .then((response) => response.json()) // This is a callback
+//     .then((users) =>
+//       this.setState(() => {
+//         // Function method
+//         return { monsters: users };
+//       })
+//     );
+// }
+
+// By extracting this function to a named function, instead of using an anonymous one, we got better code readability and performance as well
+// onSearchChange = (event) => {
+//   const searchfield = event.target.value.toLowerCase();
+//   this.setState(() => {
+//     return { searchfield };
+//   });
+// };
+
+//   render() {
+//     // We save out these objects to variables, so wont need to use 'this.' all the time
+//     const { monsters, searchfield } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) =>
+//       monster.name.toLowerCase().includes(searchfield)
+//     );
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+//         <SearchBox
+//           onChangeHandler={onSearchChange}
+//           placeholder={'search monsters'}
+//           className={'monsters-search-box'}
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
